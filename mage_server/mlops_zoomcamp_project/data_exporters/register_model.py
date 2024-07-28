d3cde5084fb8823e602df02354cad26ad22f5335
jdaguilar <jorgeaguilar2761@gmail.com>
2024-07-28 17:21:32 -0500

    change log model by register model

--- mage_server/mlops_zoomcamp_project/data_exporters/register_model.py
+++ mage_server/mlops_zoomcamp_project/data_exporters/register_model.py
@@ -18,32 +18,17 @@ if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
 
 
-mlflow.set_tracking_uri("postgresql://user:password@postgres:5432/mlflowdb")
-mlflow.set_experiment("mlops-zoomcamp-ml-exp")
-mlflow.sklearn.autolog()
-
 EXPERIMENT_NAME = "mlops-zoomcamp-ml-exp"
 
 
 @data_exporter
 def run_register_model(model, data_2, **kwargs) -> None:
-    
-    # Save Model
-    # mlflow.sklearn.log_model(model, artifact_path="models")
-
-    # pathlib.Path("models").mkdir(exist_ok=True)
-
-    #with open("models/dictvectorizer.b", "wb") as f_out:
-    #    pickle.dump(dv, f_out)
-
-    # mlflow.log_artifact("models/dictvectorizer.b", artifact_path="dictvectorizer")
-
 
     client = MlflowClient()
 
     # Retrieve the top_n model runs and log the models
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    
+
     best_run = client.search_runs(
         experiment_ids=experiment.experiment_id,
         run_view_type=ViewType.ACTIVE_ONLY,
@@ -56,8 +41,11 @@ def run_register_model(model, data_2, **kwargs) -> None:
     model_uri = f"runs:/{run_id}/model"
     mlflow.register_model(model_uri, name="clf-best-model")
 
-    model_info = mlflow.sklearn.log_model(
-        sk_model=model, 
-        artifact_path="model", 
-        # signature=signature
-    )
+    #model_info = mlflow.sklearn.log_model(
+    #    sk_model=model,
+    #    artifact_path="sk_model",
+    #    # signature=signature,
+    #    registered_model_name="clf-best-model",
+    #)
+
+    mlflow.end_run()
